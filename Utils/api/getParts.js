29ec167d2aeefc94199b5ff9193bb2a2ffd7f414
Nathan <93925029+Nath9666@users.noreply.github.com>
2024-11-27 11:23:51 +0100

    take all parts, for bdd

--- Utils/api/getParts.js
+++ Utils/api/getParts.js
@@ -1,19 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
+
+const max = 56183;
 
 const getParts = async () => {
-  const url = 'https://rebrickable.com/api/v3/lego/parts/?page=1&page_size=1000';
+  const url =
+    "https://rebrickable.com/api/v3/lego/parts/?page=1&page_size=1000";
   const headers = {
-    'Accept': 'application/json',
-    'Authorization': 'key d3dae823e10d9dd6872e861f3e2193a2'
+    Accept: "application/json",
+    Authorization: "key d3dae823e10d9dd6872e861f3e2193a2",
   };
 
   try {
     const response = await axios.get(url, { headers });
-    console.log(response.data);
     return response.data;
   } catch (error) {
-    console.error('Error fetching parts:', error);
+    console.error("Error fetching parts:", error);
   }
 };
 
-getParts().then((data) => console.log(data.results.len()));
+getParts().then((data) => {
+  if (data && data.results) {
+    console.log(`Length of data.results: ${data.results.length}`);
+  } else {
+    console.log("No results found.");
+  }
+});
