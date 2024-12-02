ebc4ac07fad4572cf150ff7bb40062eb3f150eef
Nathan <93925029+Nath9666@users.noreply.github.com>
2024-12-02 20:22:18 +0100

    fileNormal

--- Utils/DatTo3D.js
+++ Utils/DatTo3D.js
@@ -1,29 +1,11 @@
 import axios from "axios";
-import * as THREE from 'three';
-import { LDrawLoader } from 'three-stdlib/loaders/LDrawLoader.js';
-import { OBJExporter } from 'three-stdlib/exporters/OBJExporter.js';
-import { GLTFExporter } from 'three-stdlib/exporters/GLTFExporter.js';
-import fs from 'fs';
+import * as THREE from "three";
+import { LDrawLoader } from "three/examples/jsm/loaders/LDrawLoader.js";
+import { OBJExporter } from "three/examples/jsm/exporters/OBJExporter.js";
+import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter.js";
+import fs from "fs";
 
-/**
- * Fetches the 3D model URL or WebGL data for a specific LEGO part.
- *
- * @param {string} partId - The ID of the LEGO part to retrieve the 3D model URL or WebGL data for.
- * @returns {Promise<string>} A promise that resolves to the 3D model URL or WebGL data.
- * @throws Will throw an error if the fetch operation fails.
- */
-const get3DModelUrl = async (partId) => {
-  const url = `https://www.bricklink.com/3D/parts/${partId}.dat`;
-
-  try {
-    const response = await axios.get(url);
-    const data = response.data;
-    return data;
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
-};
+const exportDos = "./Models/.obj";
 
 /**
  * Converts the LDraw .dat file to a 3D model and exports it in the specified format.
@@ -32,23 +14,23 @@ const get3DModelUrl = async (partId) => {
  * @param {string} format - The format to export the 3D model to ('obj' or 'glb').
  */
 const convertAndExport3DModel = async (partId, format) => {
-  const ldrawData = await get3DModelUrl(partId);
+  const ldrawData = fs.readFileSync(`./Models/3DModels/${partId}.dat`, "utf-8");
 
   const scene = new THREE.Scene();
   const ldrawLoader = new LDrawLoader();
   ldrawLoader.parse(ldrawData, (group) => {
     scene.add(group);
 
-    if (format === 'obj') {
+    if (format === "obj") {
       const exporter = new OBJExporter();
       const result = exporter.parse(scene);
-      fs.writeFileSync(`${partId}.obj`, result);
+      fs.writeFileSync(`./Models/obj/${partId}.obj`, result);
       console.log(`3D model exported as ${partId}.obj`);
-    } else if (format === 'glb') {
+    } else if (format === "glb") {
       const exporter = new GLTFExporter();
       exporter.parse(scene, (result) => {
         const output = JSON.stringify(result);
-        fs.writeFileSync(`${partId}.glb`, output);
+        fs.writeFileSync(`./Models/obj/${partId}.glb`, output);
         console.log(`3D model exported as ${partId}.glb`);
       });
     } else {
@@ -58,4 +40,4 @@ const convertAndExport3DModel = async (partId, format) => {
 };
 
 // Example usage
-convertAndExport3DModel("3003", "obj"); // Change "obj" to "glb" to export as GLB
+convertAndExport3DModel("box", "obj"); // Change "obj" to "glb" to export as GLB
